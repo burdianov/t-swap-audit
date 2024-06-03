959e0cb4a2c0eb6b22da93230eee418b54358e04
Lilian Burdianov <lilian.burdianov@gmail.com>
2024-06-03 22:51:09 +0400

    update Handler.t.sol

--- test/invariant/Handler.t.sol
+++ test/invariant/Handler.t.sol
@@ -12,6 +12,7 @@ contract Handler is Test {
     ERC20Mock poolToken;
 
     address liquidityProvider = makeAddr("lp");
+    address swapper = makeAddr("swapper");
 
     // Gost variables
     int256 startingX;
@@ -26,6 +27,52 @@ contract Handler is Test {
         pool = _pool;
         weth = ERC20Mock(_pool.getWeth());
         poolToken = ERC20Mock(_pool.getPoolToken());
+    }
+
+    function swapPoolTokenForWethBasedOnOutputWeth(uint256 outputWeth) public {
+        outputWeth = bound(outputWeth, 0, type(uint64).max);
+        if (outputWeth >= weth.balanceOf(address(pool))) {
+            return;
+        }
+        uint256 poolTokenAmount = pool.getInputAmountBasedOnOutput(
+            outputWeth,
+            poolToken.balanceOf(address(pool)),
+            weth.balanceOf(address(pool))
+        );
+        if (poolTokenAmount > type(uint64).max) {
+            return;
+        }
+
+        startingY = int256(weth.balanceOf(address(this)));
+        startingX = int256(poolToken.balanceOf(address(this)));
+
+        expectedDeltaY = int256(-1) * int256(outputWeth);
+        expectedDeltaX = int256(
+            pool.getPoolTokensToDepositBasedOnWeth(poolTokenAmount)
+        );
+
+        if (poolToken.balanceOf(swapper) < poolTokenAmount) {
+            poolToken.mint(
+                swapper,
+                poolTokenAmount - poolToken.balanceOf(swapper) + 1
+            );
+        }
+
+        vm.startPrank(swapper);
+        poolToken.approve(address(pool), type(uint256).max);
+        pool.swapExactOutput(
+            poolToken,
+            weth,
+            outputWeth,
+            uint64(block.timestamp)
+        );
+        vm.stopPrank();
+
+        uint256 endingY = weth.balanceOf(address(this));
+        uint256 endingX = poolToken.balanceOf(address(this));
+
+        actualDeltaY = int256(endingY) - int256(startingY);
+        actualDeltaY = int256(endingX) - int256(startingX);
     }
 
     function deposit(uint256 wethAmount) public {
